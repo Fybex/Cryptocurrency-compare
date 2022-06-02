7e107da590641e4d5258716d5c93c8a3f90a4092
Andrew Valenya <fybexit@gmail.com>
2022-06-02 12:34:51 +0300

    Remove basename

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ export default function App() {
 	return (
 		<div className='App'>
 			<div className='wrapper'>
-				<HashRouter basename={process.env.PUBLIC_URL}>
+				<HashRouter>
 					<Routes>
 						<Route path='/' element={<Pairs />} />
 						<Route path='/:symbol' element={<Pairs />} />
