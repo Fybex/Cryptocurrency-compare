af320dec58141fdf3aeffb9b8f53f2ff7fb46d2d
Andrew Valenya <fybexit@gmail.com>
2022-06-02 12:29:32 +0300

    Add basename to HashRouter

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ export default function App() {
 	return (
 		<div className='App'>
 			<div className='wrapper'>
-				<HashRouter>
+				<HashRouter basename={process.env.PUBLIC_URL}>
 					<Routes>
 						<Route path='/' element={<Pairs />} />
 						<Route path='/:symbol' element={<Pairs />} />
